3c55bec979daa3130cd6cf2fffb4bd31231d8642
dhwang21 <hwang.derrick@gmail.com>
2018-12-24 12:00:12 -0800

    initalize spring

---
+++ src/test/java/com/dmd/parkingIOT/ParkingIotApplicationTests.java
@@ -0,0 +1,17 @@
+package com.dmd.parkingIOT;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ParkingIotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
