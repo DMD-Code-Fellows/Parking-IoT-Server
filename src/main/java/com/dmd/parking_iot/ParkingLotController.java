7e8b5a2db5399bca6d9c16c428b2513485f71bb1
dhwang21 <hwang.derrick@gmail.com>
2018-12-27 16:00:57 -0800

    space map

--- src/main/java/com/dmd/parking_iot/ParkingLotController.java
+++ src/main/java/com/dmd/parking_iot/ParkingLotController.java
@@ -2,6 +2,9 @@ package com.dmd.parking_iot;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Spring Boot controller class for ParkingLot class.
@@ -17,5 +20,10 @@ ParkingLotController {
     @GetMapping("/")
     public String home() {
         return "index";
+    }
+
+    @RequestMapping(value="/space-map", method= RequestMethod.GET)
+    public String showSpace(){
+        return "space";
     }
 }
